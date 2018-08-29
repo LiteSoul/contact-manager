e437aea9a0097e483acb7fb81c3d48d3b959255d
Ariel Blasi <exorcismo@gmail.com>
2018-08-29 17:36:20 -0300

    Add header & contact

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import Contact from './components/Contact'
+import Header from './components/Header'
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Main App Component</h1>
-        <input type="text"/>
+        <Header />
+        <Contact name='Juancho' email='cho@gmail.com' phone='123-456-789' />
+        <Contact name='Fabiana' email='ana@gmail.com' phone='789-456-123' />
       </div>
     );
   }
