48df1c60dd9a73e78472fc2baf7ae2ba5a3edcaf
Ariel Blasi <exorcismo@gmail.com>
2018-08-30 12:35:07 -0300

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = props => {
 	const { branding } = props
 	return (
 		<div>
-			<h1>{branding}</h1>
+			<h1 style={brandingStyle} >{branding}</h1>
 		</div>
 	)
 }
@@ -17,6 +17,8 @@ Header.defaultProps = {
 Header.propTypes = {
 	branding: PropTypes.string.isRequired
 }
+
+const brandingStyle = { color: 'red' }
 
 export default Header
 
