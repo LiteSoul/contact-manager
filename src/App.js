641c054a48cbd09c8b8a1f8e7fd0dc753d1bfbce
Ariel Blasi <exorcismo@gmail.com>
2018-09-06 19:32:32 -0300

    Form to add contact

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import Contacts from './components/Contacts'
-import Header from './components/Header'
+import Contacts from './components/contacts/Contacts'
+import AddContact from './components/contacts/AddContact'
+import Header from './components/layout/Header'
 
-import {Provider} from './context'
+import { Provider } from './context'
 
 import './App.css';
 
@@ -13,6 +14,7 @@ class App extends Component {
         <div className="App">
           <Header branding="Contact Manager" />
           <div className="container">
+            <AddContact />
             <Contacts />
           </div>
         </div>
